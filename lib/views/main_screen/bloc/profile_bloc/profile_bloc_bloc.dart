98239cb8983c451a68eef20efa9ae8316270f36f
Muneer Ahamed 01 <muneerahamed6455@gmail.com>
2023-12-15 22:23:19 +0530

    fix :changes done

--- lib/views/main_screen/bloc/profile_bloc/profile_bloc_bloc.dart
+++ lib/views/main_screen/bloc/profile_bloc/profile_bloc_bloc.dart
@@ -1,7 +1,7 @@
-import 'package:bloc/bloc.dart';
 import 'package:charge_mod/models/user_model.dart';
 import 'package:charge_mod/repositories/user_repostiroy.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'profile_bloc_event.dart';
 part 'profile_bloc_state.dart';
